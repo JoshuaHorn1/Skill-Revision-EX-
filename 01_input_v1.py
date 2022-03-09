166a9ab0692d076a025c93c673a15b6839e4c454
JoshuaHorn1 <98923400+JoshuaHorn1@users.noreply.github.com>
2022-03-10 12:00:49 +1300

    Version 1

---
+++ 01_input_v1.py
@@ -0,0 +1,21 @@
+"""Program for @home learning  - version 1
+Created by Joshua Horn
+10th March 2022
+"""
+
+# Ask user for name
+name = input("What is your name? ").capitalize()  # capitalizes the first letter of the user's name
+
+# Ask user for 2 numbers
+num_1 = input("Enter a number: ")
+num_2 = input("Enter another number: ")jo
+
+# Add numbers together
+answer = num_1 + num_2
+
+# Multiply numbers together
+
+# Greet user and display result
+print("\nHello", name) # starts output on new line
+
+print(f"{num_1} + {num_2} = {answer}".format(num_1, num_2, answer))  # prints the answer
